14c240337d08d72e6650dcf2cd5c995d3ee57a55
Maria Beatriz <mbeatriz.berten@gmail.com>
2026-08-27 09:58:48 -0300

    chore: initial spring boot project structure

---
+++ src/main/java/com/payflow/payflow_gateway/PayflowGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.payflow.payflow_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayflowGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payflow/payflow_gateway/PayflowGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payflow.payflow_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
